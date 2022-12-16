755c4a32d1f675156e00dbe02d32bf3b74bd65de
Alex Pan <alexpaofficial@gmail.com>
2022-12-15 22:18:47 -0800

    push template

--- src/column.js
+++ src/column.js
@@ -5,32 +5,9 @@ class Column extends React.Component {
     super(props);
     this.columnId = props.columnId;
   }
-  handleClicked() {
-    console.log('clicked');
-    this.props.onColumnClicked(this.columnId);
-  }
-  handleMouseEnter() {
-    this.props.onMouseEnter(this.columnId);
-  }
+  
   render() {
-    return /*#__PURE__*/React.createElement("div", {
-      className: "column",
-      onClick: this.handleClicked.bind(this),
-      onMouseEnter: this.handleMouseEnter.bind(this)
-    }, this.props.values.map((e, index) => {
-      const classList = ['innerTile'];
-      if (e === PLAYER.ONE) {
-        classList.push('player-one-tile');
-      } else if (e === PLAYER.TWO) {
-        classList.push('player-two-tile');
-      }
-      return /*#__PURE__*/React.createElement("div", {
-        key: index,
-        className: "tile"
-      }, /*#__PURE__*/React.createElement("div", {
-        className: classList.join(' ')
-      }));
-    }));
+    // FILL THIS IN
   }
 }
 export default Column;
--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 import React from './react';
 import Column from './column.js';
-import Dropper from './dropper.js';
 import { PLAYER } from './constants';
+
 class Game extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +11,7 @@ class Game extends React.Component {
       player: PLAYER.ONE
     };
   }
+
   getEmptyBoard() {
     const tiles = [];
     for (let i = 0; i < this.props.columns; ++i) {
@@ -22,44 +23,9 @@ class Game extends React.Component {
     }
     return tiles;
   }
-  onColumnClicked(columnId) {
-    this.setState(state => {
-      const copiedTiles = state.tiles.map(e => [...e]);
-      for (let i = copiedTiles[columnId].length - 1; i >= 0; --i) {
-        if (copiedTiles[columnId][i] === 0) {
-          copiedTiles[columnId][i] = state.player;
-          break;
-        }
-      }
-      return {
-        tiles: copiedTiles,
-        player: state.player === PLAYER.ONE ? PLAYER.TWO : PLAYER.ONE
-      };
-    });
-    console.log('clicked', this.state);
-  }
-  onMouseEnter(columnId) {
-    this.setState({
-      columnHovered: columnId
-    });
-  }
+
   render() {
-    return /*#__PURE__*/React.createElement("div", {
-      id: "game-wrapper"
-    }, /*#__PURE__*/React.createElement(Dropper, {
-      colorClass: this.state.player === PLAYER.ONE ? 'player-one-tile' : 'player-two-tile',
-      columnHovered: this.state.columnHovered
-    }), /*#__PURE__*/React.createElement("div", {
-      id: "board"
-    }, this.state.tiles.map((e, index) => {
-      return /*#__PURE__*/React.createElement(Column, {
-        onColumnClicked: this.onColumnClicked.bind(this),
-        onMouseEnter: this.onMouseEnter.bind(this),
-        values: e,
-        columnId: index,
-        key: index.toString()
-      });
-    })));
+    // FILL This IN
   }
 }
 export default Game;
